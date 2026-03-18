830b40ec9654115089aa9b7c5dc905f7272acdaf
Warsha97 <warkiringoda@gmail.com>
2026-03-18 19:25:12 +0100

    Adds code for prompting from the llm

--- LocalAI/Program.cs
+++ LocalAI/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.AI;
+using OllamaSharp;
+
+IChatClient chatClient =
+    new OllamaApiClient(new Uri("http://localhost:11434/"), "phi3:mini");
+
+// Start the conversation with context for the AI model
+List<ChatMessage> chatHistory = new();
+
+while (true)
+{
+    // Get user prompt and add to chat history
+    Console.WriteLine("Your prompt:");
+    var userPrompt = Console.ReadLine();
+    chatHistory.Add(new ChatMessage(ChatRole.User, userPrompt));
+
+    // Stream the AI response and add to chat history
+    Console.WriteLine("AI Response:");
+    var response = "";
+    await foreach (ChatResponseUpdate item in
+        chatClient.GetStreamingResponseAsync(chatHistory))
+    {
+        Console.Write(item.Text);
+        response += item.Text;
+    }
+    chatHistory.Add(new ChatMessage(ChatRole.Assistant, response));
+    Console.WriteLine();
+}
